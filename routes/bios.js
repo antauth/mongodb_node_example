225c1375a4870fc821e2847ffd1a4e86c00f2374
Antoinette Smith <antoinette@primeacademy.io>
2015-12-02 03:00:39 -0600

    Added a put request to add awards

--- routes/bios.js
+++ routes/bios.js
@@ -2,13 +2,19 @@ var express = require('express');
 var router = express.Router();
 var mongo = require('../data/mongo');
 
-/* GET home page. */
+/* GET a specific bio by ID */
 router.get('/:id', function(req, res, next) {
   var results = mongo.bios.find({'_id': parseInt(req.params.id)}).limit(1); //cursor of results
   var array = results.toArray(function(err, arr) {
     var doc = arr[0];
     res.json(doc);
   });
+});
+
+router.put('/award/:id', function(req, res, next) {
+  console.log(req.body);
+  var results = mongo.bios.update({'_id': parseInt(req.params.id)}, {'$push':{'awards':req.body.award}}, false, false);
+  res.json(results);
 });
 
 module.exports = router;
